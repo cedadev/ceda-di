83ccdfb3e57c1038ef7ab764b6581139b9bd5515
Charles Newey <charlie@assemblyco.de>
2014-07-10 11:36:16 +0100

    Add tools directory for miscellaneous useful data-exploring code

---
+++ tools/nc_vardump.py
@@ -0,0 +1,19 @@
+﻿#! /usr/bin/env python
+
+from netCDF4 import Dataset
+import json
+import os
+import sys
+
+for arg in sys.argv[1:]:
+    output_fname = os.path.splitext(os.path.basename(arg))[0]
+    output_fname += ".json"
+    with open(output_fname, 'w') as f:
+        with Dataset(arg, 'r') as nc:
+            f.write(json.dumps(nc.variables,
+                               indent=4,
+                               sort_keys=True,
+                               default=repr))
+                               
+            for k, v in nc.variables.iteritems():
+                print k, type(v), v[0]
