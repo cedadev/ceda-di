d070e887f3be82235336aa5b9b8e1c37004f8408
Charles Newey <charlie@assemblyco.de>
2014-07-11 15:38:22 +0100

    Add base datafile class

---
+++ eufar/_datafile.py
@@ -0,0 +1,13 @@
+﻿#! /usr/bin/env python
+
+class _geospatial(object):
+    # TODO
+    def __init__(self):
+        exception = \
+        "_datafile is an abstract base class and is intended \
+        to be used as an interface for EUFAR data files ONLY."
+
+        raise NotImplementedError(exception)
+
+    def get_geospatial_dict(self):
+        __init__()
