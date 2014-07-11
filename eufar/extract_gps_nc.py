3e85af87348518c99e141b15fcfd89d37c89fade
Charles Newey <charlie@assemblyco.de>
2014-07-11 15:03:26 +0100

    Refactor and improve compatibility for extract_gps_nc.py

--- eufar/extract_gps_nc.py
+++ eufar/extract_gps_nc.py
@@ -9,18 +9,21 @@ import os
 import re
 import sys
 import time
+import tools.nc_dump
 
 # TODO move this lot to a config file
 TIME_FORMATS = ["seconds since %Y-%m-%d %H:%M:%S %z",
                 "seconds since %Y-%m-%d %H:%M:%S %Z",
                 "seconds since %Y-%m-%d %H:%M:%S 00:00 %Z"]
-
-LAT_NAMES = ["LAT_GIN", "pos_lat_gps_25", "pos_lat_gps_1",
-             "pos_lat_airinspp_1", "GPS_LAT_NP"]
-LON_NAMES = ["LON_GIN", "pos_lon_gps_25", "pos_lon_gps_1",
-             "pos_lon_airinspp_1", "GPS_LON_NP"]
-ALT_NAMES = ["ALT_GIN", "alt_alti_gps_25", "alt_alti_gps_1",
-             "alt_airinspp_1", "GPS_ALT_NP"]
+                
+LAT_NAMES = ["GPS_LAT", "LAT_GIN", "pos_lat_gps_25", "pos_lat_gps_1",
+             "pos_lat_airinspp_1", "GPS_LAT_NP", "Lat", "latitude"]
+             
+LON_NAMES = ["GPS_LON", "LON_GIN", "pos_lon_gps_25", "pos_lon_gps_1",
+             "pos_lon_airinspp_1", "GPS_LON_NP", "Lon", "longitude"]
+             
+ALT_NAMES = ["ALTITUDE", "ALT_GIN", "alt_alti_gps_25", "alt_alti_gps_1",
+             "alt_airinspp_1", "GPS_ALT_NP", "Alt", "height_above_sea_level"]
 
 
 def _get_netcdf_var_from_regex(regex, nc, flags=None):
@@ -48,41 +51,66 @@ def _get_netcdf_var_from_list(var_list, nc):
     for var in var_list:
             if var in nc.variables:
                 return var
-    return None
+                
+    raise AttributeError("Could not find attribute in list: %s", var_list)
 
 
-def _time_from_gps_format(nc, tm_var):
-    tm_list = list(nc.variables[tm_var][:].ravel())
+def _time_from_num_format(nc, tm_var):
+    """
+    """
+    shape = nc.variables[tm_var].shape[0]
+    if shape == 1:
+        tm_list = list(nc.variables[tm_var][:].ravel())
+    else:
+        # If this is reached, the times are in a strange numpy.ndarray
+        # Here, we convert to datetime objects (with a small loss of precision)
+        times = list(nc.variables[tm_var][:].ravel(order='F'))
+        tm_list = []
+        for i in xrange(0, len(times), shape):
+            time = []
+            for j in xrange(0, shape):
+                time.append(int(times[i + j]))
+            tm_list.append(dt(*time))
+            
+        return tm_list
+        
+    if hasattr(nc.variables[tm_var], "units"):
+        # Tries to parse from epoch time
+        tm_str = nc.variables[tm_var].units
+    elif hasattr(nc, "date"):
+        # Tries to parse from GPS time
+        nc_date = re.match(r"(\d{2})/(\d{2})/(\d{4})", nc.date).groups()
+        tm_str = ("seconds since %s-%s-%s 00:00:00 UTC" % 
+                     (nc_date[2], nc_date[1], nc_date[0]))
 
-    nc_date = re.match(r"(\d{2})/(\d{2})/(\d{4})", nc.date).groups()
-    tm_str = "seconds since %s-%s-%s 00:00:00 UTC" % (nc_date[2],
-                                                      nc_date[1],
-                                                      nc_date[0])
-    base_time = dt.strptime(tm_str, TIME_FORMATS[1])
-    secs_past_base_time = tm_list
-    timestamps = [base_time +
-                  timedelta(seconds=float(sec))
-                  for sec in secs_past_base_time]
-
-    return timestamps
+    try:
+        # Try converting the epoch/GPS timestamps to datetime objects here
+        base_time = dt.strptime(tm_str, TIME_FORMATS[1])
+        timestamps = [base_time +
+                      timedelta(seconds=float(sec))
+                      for sec in tm_list]
+        return timestamps
+    except ValueError:
+        # I tried :( couldn't work out what times were, return empty list
+        return []
 
 
 def _time_from_str_format(nc, tm_var):
+    """
+    """
     for tm_str in TIME_FORMATS:
         try:
+            tm_list = nc.variables[tm_var]
             base_time = dt.strptime(str(tm_list[0]), tm_str)
-            secs_past_base_time = tm_list
-            timestamps = [base_time +
-                          timedelta(seconds=int(sec))
-                          for sec in secs_past_base_time]
-
+            timestamps = [base_time + timedelta(seconds=int(sec))
+                             for sec in tm_list]
             return timestamps
         except ValueError:
             # Variable isn't in the format we tried, so try next alternative
             continue
 
-    # Raise error if time couldn't be parsed
-    raise ValueError("Could not match format for time: %s" % tm)
+    # Raise error if list is exhausted
+    raise ValueError("Could not match format for time: %s" % tm_list[0])
 
 
 def get_time_data(nc):
@@ -90,19 +118,21 @@ def get_time_data(nc):
     """
     # If base_time isn't in the format we expect, we may have to calculate it
     tm_var = _get_netcdf_var_from_regex("time", nc, flags=re.IGNORECASE)
-    datum = nc.variables[tm_var][0][0]
+    datum = nc.variables[tm_var][0]
 
+    # If 'datum' is a numpy n-dimensional array then check type of first item
+    if isinstance(datum, numpy.ndarray):
+        datum = datum.flat[0]
+    
+    # Is time a numeric time format? (epoch, GPS or Julian)
     if isinstance(datum, numpy.float32) or isinstance(datum, numpy.float64):
-        return _time_from_gps_format(nc, tm_var)
-    else:
+        return _time_from_num_format(nc, tm_var)
+    # If string, then time is timestamp
+    elif isinstance(datum, str):
         return _time_from_str_format(nc, tm_var)
 
 
-def get_lat_lon_alt_data(nc):
-    raise ValueError("Not implemented")
-
-
-def raw_gps_safire(fname):
+def get_geospatial(fname):
     """
     Opens specified NetCDF file and extracts lat/lon/alt/timestamp data.
 
@@ -123,23 +153,26 @@ def raw_gps_safire(fname):
             alt = netcdf_data.variables[alt_var][:].ravel()[10:-10]
 
             finfo = {
-                "filename": fname,
+                "filename": os.path.basename(fname),
                 "length": len(timestamps),
                 "lon": [float(x) for x in lon],
                 "lat": [float(x) for x in lat],
                 "alt": [float(x) for x in alt],
-                "time": [dt.isoformat(x) for x in timestamps]
+                "time":[dt.isoformat(x) for x in timestamps]
             }
 
             return finfo
         except KeyError as k:
-            print("KeyError: %s (%s)" % (str(k), fname))
+            tools.nc_dump.dump(netcdf_data)
+            raise("\nKeyError: %s (%s)" % (str(k), fname))
 
 
-for fname in sys.argv[1:]:
-    gps_info = raw_gps_safire(fname)
-    print(fname)
+if __name__ == "__main__":
+    for fname in sys.argv[1:]:
+        gps_info = get_geospatial(fname)
+        print(fname)
 
-    out_name = "out/" + os.path.splitext(os.path.basename(fname))[0] + ".json"
-    with open(out_name, 'w') as f:
-        f.write(json.dumps(gps_info, indent=4))
+        out_name = "out/" + os.path.splitext(
+                                os.path.basename(fname))[0] + ".json"
+        with open(out_name, 'w') as f:
+            f.write(json.dumps(gps_info, indent=4))
