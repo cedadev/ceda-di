6550eb5e6820f8277fc8a643efc99f58c696a173
Charles Newey <charlie@assemblyco.de>
2014-07-15 11:35:18 +0100

    Create sensible constructor for envi_geo.BIL
    Create sensible constructor for envi_geo.BIL

--- eufar/envi_geo.py
+++ eufar/envi_geo.py
@@ -3,15 +3,16 @@ from _dataset import _geospatial
 
 
 class BIL(_geospatial):
-    def get_geospatial(self, header_fpath):
+    def __init__(self, header_path, path=None, unpack_fmt="<d"):
+        self.b = envi.BilFile(header_path, path, unpack_fmt)
+
+    def get_geospatial(self):
         """
         :param str header_fpath: Filename of header file
         :return dict: A dict containing geospatial and temporal information
         """
 
-        b = envi.BilFile(header_fpath)
         bil = b.read_bil()
-
         swath_path = {
             "lines": b.hdr["lines"],
             "time": bil[0],
