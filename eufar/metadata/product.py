a1e1ee7f0374d03998340104e5b9025edd99f76a
Charles Newey <charlie@assemblyco.de>
2014-07-21 15:52:01 +0100

    Add outline for metadata product class

---
+++ eufar/metadata/product.py
@@ -0,0 +1,37 @@
+import json
+
+
+class Properties(object):
+    def __init__(self, file_level=None, spatial=None,
+                 temporal=None, parameters=None, data_format=None):
+        """
+        Construct a eufar.metadata.Properties object with data conforming to
+        Steve Donegan's FatCat JSON metadata structure
+        (see "arsf-geo-map/doc/schema.json")
+        """
+
+        self.file_level = file_level
+        self.spatial = spatial
+        self.temporal = temporal
+        self.parameters = parameters
+        self.data_format = data_format
+
+    def to_json():
+        properties = {
+            "file": {
+                "properties": self.file_level,
+            },
+            "spatial": {
+                "properties": self.spatial,
+            },
+            "temporal": {
+                "properties": self.temporal,
+            },
+            "parameters": {
+                "properties": self.parameters,
+            },
+            "data_format": {
+                "properties": self.data_format,
+            },
+        }
+        return json.dumps(properties, indent=4)
