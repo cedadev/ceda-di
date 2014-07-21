24a9c0d727a0ad8fc95ecd7b6523fb801f2cb229
Charles Newey <charlie@assemblyco.de>
2014-07-21 17:40:42 +0100

    Add method for fetching file-level metadata

--- eufar/_dataset.py
+++ eufar/_dataset.py
@@ -1,3 +1,6 @@
+import os
+
+
 class _geospatial(object):
     def __init__(self):
         exception = \
@@ -5,6 +8,13 @@ class _geospatial(object):
             "to be used as an interface for EUFAR data files ONLY."
 
         raise NotImplementedError(exception)
+
+    def get_file_level(self, fpath):
+        file_level = {
+            "filename": os.path.basename(fpath),
+            "path": self.path,
+            "size": os.stat(fpath).st_size,
+        }
 
     def get_geospatial_dict(self):
         self.__init__()
--- eufar/envi_geo.py
+++ eufar/envi_geo.py
@@ -5,17 +5,18 @@ from _dataset import _geospatial
 class ENVI(_geospatial):
     def __init__(self):
         raise NotImplementedError("Do not instantiate this class. Use BIL/BSQ.")
-        
+
+    def _load_data(self):
+        if not self.data:
+            self.b.read()
+
     def get_geospatial(self):
         """
-        :param str header_fpath: Filename of header file
         :return dict: A dict containing geospatial and temporal information
         """
-        
-        data = self.b.read()
-        swath_path = {
-            "lines": self.b.hdr["lines"],
-            "time": data[0],
+
+        self._load_data()
+        spatial = {
             "lat": data[1],
             "lon": data[2],
             "alt": data[3],
@@ -24,11 +25,29 @@ class ENVI(_geospatial):
             "heading": data[6]
         }
 
-        return swath_path
+        return spatial
+
+    def get_temporal(self):
+        self._load_data()
+        temporal = {
+            "start_time": self.data[0][0],
+            "end_time": self.data[0][-1],
+        }
+
+        return temporal
+
+    def get_data_format(self):
+        data_format = {
+            "format": self.data_format,
+        }
+
+        return data_format
+
 
 class BIL(ENVI):
     def __init__(self, header_path, path=None, unpack_fmt="<d"):
         self.header_path = header_path
+        self.data_format = "ENVI BIL (Band Interleaved by Line)"
         self.path = path
         self.unpack_fmt = unpack_fmt
 
@@ -44,11 +63,12 @@ class BIL(ENVI):
 
     def __exit__(self, *args):
         pass
-        
+
 
 class BSQ(ENVI):
     def __init__(self, header_path, path=None, unpack_fmt="<d"):
         self.header_path = header_path
+        self.data_format = "ENVI BSQ (Band Sequential)"
         self.path = path
         self.unpack_fmt = unpack_fmt
 
