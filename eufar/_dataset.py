80a9e07fd06220da3180ebc2bfaec127969e3bd0
Charles Newey <charlie@assemblyco.de>
2014-07-22 09:56:37 +0100

    Fix file-level information method

--- eufar/_dataset.py
+++ eufar/_dataset.py
@@ -1,20 +1,25 @@
+import os
+
+
 class _geospatial(object):
     def __init__(self):
         exception = \
-            "\"_datafile\" is an abstract base class and is intended" + \
-            "to be used as an interface for EUFAR data files ONLY."
+            "\"_geospatial\" is an abstract base class and is intended" + \
+            "to be used as an OO interface for EUFAR data files ONLY."
 
         raise NotImplementedError(exception)
 
     def get_file_level(self, fpath):
         file_level = {
             "filename": os.path.basename(fpath),
-            "path": self.path,
+            "path": fpath,
             "size": os.stat(fpath).st_size,
         }
+        
+        return file_level
 
 	def get_geospatial(self):
-        self.__init__()
+	    self.__init__()
 
     def get_temporal(self):
         self.__init__()
