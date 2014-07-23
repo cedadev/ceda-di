db2a6399ab752453005262d9f7e16ab4e746a6fc
Charles Newey <charlie@assemblyco.de>
2014-07-23 13:32:37 +0100

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -3,20 +3,31 @@ import sys
 from eufar import envi_geo
 
 
+def write_properties(fname, _geospatial_obj):
+    fname = "out/%s.json" % os.splitext(fname)[0]  # Create JSON path
+    with open(fname, 'w') as j:
+        props = str(_geospatial_obj.get_properties())
+        j.write(props)
+
+
 def process_bil(path):
     with envi_geo.BIL(path) as b:
-        json_fn = ["out/", os.path.splitext(f)[0], ".json"]
-        with open(''.join(json_fn), 'w') as j:
-            j.write(str(b.get_properties()))
+        json_fn = write_properties(f)
 
 
-process_counter = 0
-for root, dirs, files in os.walk("/badc/eufar/data/aircraft/", followlinks=True):
-    for f in files:
-        if f.endswith(".hdr") and "nav" in f and "qual" not in f:
-            path = os.path.join(root, f)
-            process_bil(path)
+if __name__ == "__main__":
+    if not os.path.isdir("out"):
+        os.mkdir("out")
 
-            process_counter += 1
-            if (process_counter % 100) == 0:
-                sys.stdout.write("%d files processed\n" % process_counter)
+    file_counter = 0
+    start_path = "/badc/eufar/data/aircraft/"
+    for root, dirs, files in os.walk(start_path, followlinks=True):
+        for f in files:
+            # BIL navigation files
+            if f.endswith(".hdr") and "nav" in f and "qual" not in f:
+                path = os.path.join(root, f)
+                process_bil(path)
+
+                file_counter += 1
+                if not (file_counter % 20):
+                    sys.stdout.write("%d files processed\n" % file_counter)
