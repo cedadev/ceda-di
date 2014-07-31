a72c3bc74dc8d8b1ef4c566edbd17f2b0a1454b0
Charles Newey <charlie@assemblyco.de>
2014-07-31 11:33:19 +0100

    Remove redundant tests (not unit tests)

--- test/test_envi.py
+++
@@ -1,7 +0,0 @@
-import eufar.envi_geo as envi
-import json
-
-bil = envi.BIL("/home/ccnewey/Downloads/e098021b_nav_post_processed.bil.hdr")
-
-with open("out.json", 'w') as j:
-    j.write(json.dumps(bil.get_geospatial(), indent=4))
--- test/test_nc.py
+++
@@ -1,7 +0,0 @@
-from eufar import netcdf_geo
-import json
-import sys
-
-n = netcdf_geo.NetCDF(sys.argv[1])
-with open(sys.argv[1]+".json", 'w') as f:
-    f.write(json.dumps(n.get_geospatial(), indent=4))
