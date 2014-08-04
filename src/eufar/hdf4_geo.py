21817e39bf18ec7dcb71da005fdca8b3f635fe3a
Charles Newey <charlie@assemblyco.de>
2014-08-04 13:08:34 +0100

    Add timestamp parsing methods

--- src/eufar/hdf4_geo.py
+++ src/eufar/hdf4_geo.py
@@ -1,6 +1,13 @@
-from pyhdf.HDF import *
-from pyhdf.V import *
-from pyhdf.VS import *
+"""
+Interface to extract and generate JSON from HDF4 EUFAR metadata
+"""
+
+import datetime
+
+from pyhdf.HDF import HDF
+from pyhdf.VS import VS
+from pyhdf.V import V
+from pyhdf.error import HDF4Error
 
 from _dataset import _geospatial
 from metadata import product
@@ -8,8 +15,12 @@ from metadata import product
 
 class HDF4(_geospatial):
     """
-    ARSF HDF4 context manager class.
+    ARSF/EUFAR metadata HDF4 context manager class.
     """
+    hdf = None
+    vs = None
+    v = None
+
     def __init__(self, fname):
         """
         :param str fname: The path of the HDF4 file.
@@ -65,12 +76,55 @@ class HDF4(_geospatial):
             vd.detach()
         return coords
 
+    def _get_temporal(self, v, vs, fn):
+        """
+        Returns start and end timestamps (if existing)
+        """
+        mappings = {
+            "MIdate": "date",
+            "MIstime": "start_time",
+            "MIetime": "end_time",
+        }
+
+        timestamps = {}
+        for k, v in mappings.iteritems():
+            ref = vs.find(k)
+            vd = vs.attach(ref)
+
+            timestamps[v] = []
+            while True:
+                try:
+                    timestamps[v].append(vd.read()[0][0])
+                except HDF4Error:  # EOF
+                    break
+
+            vd.detach()
+
+        # Convert date from list of integers to string (because HDF is weird)
+        timestamps["date"] = ''.join([chr(x) for x in timestamps["date"]
+                                      if x != 0])
+        return timestamps
+
+    def _parse_timestamp(self, tm_dict):
+        """
+        Parse start and end timestamps from an HDF4 file.
+
+        :param dict tm_dict: The timestamp to be parsed
+        """
+        st_base = ("%s %s", tm_dict["date"], tm_dict["start_time"])
+        et_base = ("%s %s", tm_dict["date"], tm_dict["end_time"])
+
+        start_time = datetime.datetime.strptime(st_base, "%d/%m/%Y %H%M%S")
+        end_time = datetime.datetime.strptime(et_base, "%d/%m/%Y %H%M%S")
+        return {"start_time": start_time,
+                "end_time": end_time}
+
     def get_geospatial(self):
         """
         Search through HDF4 file, returning a list of coordinates from the
         'Navigation' vgroup (if it exists).
 
-        :return dict: Dict containing geospatial and temporal information.
+        :return dict: Dict containing geospatial information.
         """
         ref = -1
         while True:
@@ -84,14 +138,43 @@ class HDF4(_geospatial):
                     return geospatial
 
                 vg.detach()
-            except HDF4Error:
+            except HDF4Error:  # End of file
+                # This is a weird way of handling files, but this is what the
+                # pyhdf library demonstrates...
                 break
 
+        return None
+
     def get_temporal(self):
-        # TODO
+        """
+        Search through HDF4 file, returning timestamps from the 'Mission'
+        vgroup (if it exists)
+        """
+        ref = -1
+        while True:
+            try:
+                ref = self.v.getid(ref)
+                vg = self.v.attach(ref)
+
+                if vg._name == "Mission":
+                    temporal = self._get_temporal(self.v, self.vs, self.fname)
+                    vg.detach()
+                    return temporal
+
+                vg.detach()
+            except HDF4Error:  # End of file
+                # This 'except at end of file' thing is some pyhdf weirdness
+                # Check the pyhdf documentation for clarification
+                break
+
         return None
 
     def get_properties(self):
+        """
+        :return eufar.metadata.properties.Properties: Metadata object
+        Returns eufar.metadata.properties.Properties object
+        containing geospatial and temporal metadata from file.
+        """
         geospatial = self.get_geospatial()
         temporal = self.get_temporal()
         file_level = super(HDF4, self).get_file_level(self.fname)
