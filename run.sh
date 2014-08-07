03e3d10bcc8d9f82fd2b0d19cf43ce026bdf302a
Charles Newey <charlie@assemblyco.de>
2014-08-07 10:56:04 +0100

    Update BSUB parameters in 'run.sh'

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-#BSUB -n 45
-#BSUB -W 1:15
-#BSUB -M 1048576
-#BSUB -q lotus-smp
-#BSUB -N charles.newey@stfc.ac.uk
-#BSUB -o %J.out
+#BSUB -n 10
+#BSUB -W 2:45
+#BSUB -q lotus
 
 cd ~ccnewey/arsf-geo-map
 source bin/activate
