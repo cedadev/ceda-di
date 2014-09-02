9a92fa9858c19c61bd9710e53850dec89c4fb76c
Charles Newey <charlie@assemblyco.de>
2014-09-02 16:05:48 +0100

    Unify configuration options

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ Main script to handle processing of EUFAR data.
 """
 
 import datetime
+import json
 import logging
 import logging.config
 import multiprocessing
@@ -12,18 +13,26 @@ import sys
 from eufar import envi_geo, exif_geo, hdf4_geo, netcdf_geo
 
 
-BASE_DIR = "/work/scratch/ccnewey/arsf-geo-map/"
-LOG_SUFFIX = "log/"
-OUTPUT_SUFFIX = "out/"
+def get_config(fname="config/eufar.json"):
+    """
+    Reads the configuration file into a dictionary
+    :param fname: Path to the JSON configuration file
+    :return dict: Dict containing parsed JSON config
+    """
+    try:
+        with open(fname, "r") as f:
+            return json.load(f)
+    except IOError as ioe:
+        sys.stderr.write("ERR: can't read config.\n\t%s\n" % str(ioe))
+        exit(1)
 
-LOGPATH = os.path.join(BASE_DIR, LOG_SUFFIX)
-OUTPATH = os.path.join(BASE_DIR, OUTPUT_SUFFIX)
+
 def write_properties(fname, _geospatial_obj):
     """Write module properties to an output file."""
     fname = os.path.basename(fname)
 
     # Construct JSON path
-    fname = "%s/%s.json" % (OUTPATH, os.path.splitext(fname)[0])
+    fname = "%s/%s.json" % (jsonpath, os.path.splitext(fname)[0])
 
     with open(fname, 'w') as j:
         props = str(_geospatial_obj.get_properties())
@@ -38,7 +47,7 @@ def process_bil(fpath):
 
 def process_hdf4(fpath):
     """Process HDF4 files."""
-    with hdf4_geo.HDF4(fpath) as hdf:
+    with hdf4_geo.HDF4(str(fpath)) as hdf:
         write_properties(fpath, hdf)
 
 
@@ -55,60 +64,74 @@ def process_tiff(fpath):
         write_properties(fpath, exif)
 
 
-def prepare_logging():
+def prepare_logging(conf):
     """Initial logging setup"""
-    if not os.path.isdir(LOGPATH):
-        os.makedirs(LOGPATH)
+    if not os.path.isdir(conf["logpath"]):
+        os.makedirs(conf["logpath"])
 
-    logging.config.fileConfig("logging.conf")
-    log = logging.getLogger("main")
+    fname = os.path.join(conf["logpath"], conf["logfile"])
+    logging_config = {
+        "filename": fname,
+        "format": conf["logging"]["format"],
+        "level": logging.INFO
+    }
+
+    logging.basicConfig(**logging_config)
+    log = logging.getLogger(__name__)
 
     return log
 
-
+jsonpath = ""
 if __name__ == "__main__":
-    # Cores
-    NUMCORES = int(sys.argv[1])
+    # Config
+    if len(sys.argv) > 1:
+        config = get_config(sys.argv[1])
+    else:
+        config = get_config()
 
-    # Set up logging
-    LOGGER = prepare_logging()
+    # File paths
+    try:
+        logger = prepare_logging(config)
+        numcores = config["numcores"]
+        datapath = config["datapath"]
+        outpath = config["outputpath"]
+        jsonpath = os.path.join(outpath, config["jsonpath"])
 
-    # Make output directory for JSON
-    if not os.path.isdir(OUTPATH):
-        os.makedirs(OUTPATH)
+        if not os.path.isdir(jsonpath):
+            os.makedirs(jsonpath)
+    except KeyError as k:
+        sys.stderr.write("Missing configuration option: %s" % str(k))
 
-    START_TIME = datetime.datetime.now()
-    LOGGER.info("Metadata extraction started at: %s" % START_TIME.isoformat())
-
-    START_PATH = "/badc/eufar/data/projects/"
+    start = datetime.datetime.now()
+    logger.info("Metadata extraction started at: %s", start.isoformat())
     processes = []
-    for root, dirs, files in os.walk(START_PATH, followlinks=True):
+    for root, dirs, files in os.walk(datapath, followlinks=True):
         for f in files:
             path = os.path.join(root, f)
 
             if "raw" not in path:
                 if f.endswith("_nav_post_processed.bil.hdr"):
                     proc = multiprocessing.Process(target=process_bil,
-                                               args=(path,))
+                                                   args=(path,))
                     processes.append(proc)
                     proc.start()
                 elif f.endswith(".nc"):
                     proc = multiprocessing.Process(target=process_nc,
-                                               args=(path,))
+                                                   args=(path,))
                     processes.append(proc)
                     proc.start()
                 elif f.endswith(".tif"):
                     proc = multiprocessing.Process(target=process_tiff,
-                                               args=(path,))
+                                                   args=(path,))
                     processes.append(proc)
                     proc.start()
                 elif f.endswith(".hdf"):
                     proc = multiprocessing.Process(target=process_hdf4,
-                                               args=(path,))
+                                                   args=(path,))
                     processes.append(proc)
                     proc.start()
 
-            while len(processes) > NUMCORES:
+            while len(processes) > numcores:
                 for p in processes:
                     if p.exitcode is not None:
                         processes.remove(p)
@@ -117,8 +140,9 @@ if __name__ == "__main__":
     for proc in processes:
         proc.join()
 
-    END_TIME = datetime.datetime.now()
-    LOGGER.info("Metadata extraction completed at: %s" % END_TIME.isoformat())
-    LOGGER.info("Start: %s, End: %s, Total: %s" % (START_TIME.isoformat(),
-                                                   END_TIME.isoformat(),
-                                                   (END_TIME - START_TIME)))
+    end = datetime.datetime.now()
+    logger.info("Metadata extraction completed at: %s", end.isoformat())
+    logger.info("Start: %s, End: %s, Total: %s",
+                start.isoformat(),
+                end.isoformat(),
+                end - start)
