bcf6ee40bd3344f072511e1cfadd23b88a6179b2
Charles Newey <charlie@assemblyco.de>
2014-09-08 17:01:46 +0100

    Refactor bad __init__ call in eufar._dataset

--- src/eufar/_dataset.py
+++ src/eufar/_dataset.py
@@ -1,20 +1,18 @@
-"""
-Base class used as interface for providing file-level metadata from geospatial
-and temporal metadata files.
-"""
-
+"""Base class for metadata wrapper classes."""
 import os
 
 
 class _geospatial(object):
+    """
+    Base class used as interface for providing file-level metadata from
+    geospatial and temporal metadata files.
+    """
     def __init__(self):
-        exception = \
-            "\"_geospatial\" is an abstract base class and is intended" + \
-            "to be used as an OO interface for EUFAR data files ONLY."
+        self.unimpl_base_class()
 
-        raise NotImplementedError(exception)
-
-    def get_file_level(self, fpath):
+    @staticmethod
+    def get_file_level(fpath):
+        """Returns a dict containing filesystem information"""
         file_level = {
             "filename": os.path.basename(fpath),
             "path": fpath,
@@ -24,10 +22,23 @@ class _geospatial(object):
         return file_level
 
     def get_geospatial(self):
-        self.__init__()
+        """Returns a dict containing geospatial information"""
+        self.unimpl_base_class()
 
     def get_temporal(self):
-        self.__init__()
+        """Returns a dict containing temporal information"""
+        self.unimpl_base_class()
 
     def get_parameters(self):
-        self.__init__()
+        """Returns a dict containing parameter information"""
+        self.unimpl_base_class()
+
+    @staticmethod
+    def unimpl_base_class():
+        """Throws relevant NotImplementedError."""
+        exception = \
+            "\"_geospatial\" is an abstract base class and is intended" + \
+            "to be used as an OO interface for EUFAR data files ONLY."
+
+        raise NotImplementedError(exception)
+
