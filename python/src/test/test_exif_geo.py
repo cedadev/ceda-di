e47baf6bddd2b0d3cf85587e32bff5e7d74dcc95
Charles Newey <charlie@assemblyco.de>
2014-09-18 14:54:14 +0100

    Add outline tests for exif_geo

---
+++ python/src/test/test_exif_geo.py
@@ -0,0 +1,46 @@
+"""
+Test module for eufar.exif_geo
+"""
+
+import unittest
+
+from eufar.exif_geo import EXIF
+
+class TestEXIF(unittest.TestCase):
+    """Test class for eufar.exif_geo.EXIF"""
+    def setUp(self):
+        self.path = "/path/to/some/file"
+        self.xml = {
+            "Camera_Image": {
+                "Plane_info": {
+                    "Exterior_orientation": {
+                        "Position": {
+                            "Latitude": 1.234,
+                            "Longitude": 4.321,
+                            "Height": 9.876
+                        }
+                    }
+                },
+                "Project_info": {
+                    "Year": 1901,
+                    "Flight_day_of_year": 5,
+                    "GPStime_of_week": 86401
+                }
+            }
+        }
+
+
+    def test_get_geospatial(self):
+        with EXIF(self.path) as exif_file:
+            exif_file.xml = self.xml
+            assert exif_file.get_geospatial() == {
+                "lat": [1.234],
+                "lon": [4.321],
+                "alt": [9.876]
+            }
+
+    def test_get_temporal(self):
+        with EXIF(self.path) as exif_file:
+            exif_file.xml = self.xml
+            s_time = exif_file.get_temporal()["start_time"]
+            assert s_time == "1901-01-05T00:00:01"
