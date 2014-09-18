e81c26d0ee3563dba5df270ecf8fac1b0feb4065
Charles Newey <charlie@assemblyco.de>
2014-09-18 10:44:30 +0100

    Add some tests for netcdf_geo

---
+++ python/src/test/__init__.py
@@ -0,0 +1,5 @@
+import sys
+import os
+
+module_path = os.path.abspath("../")
+sys.path.insert(0, module_path)
--- python/src/test/test_netcdf_geo.py
+++ python/src/test/test_netcdf_geo.py
@@ -2,37 +2,74 @@
 Test module for eufar.netcdf_geo
 """
 
-import os
+import re
 import unittest
+from mock import Mock
 
-
-from netCDF4 import Dataset
-from eufar import netcdf_geo
-
+from eufar.netcdf_geo import NetCDF
+from test import netCDF4
 
 class TestNetCDF(unittest.TestCase):
     """
     Test class for eufar.netcdf_geo.NetCDF
     """
     def setUp(self):
-        # Hardwire in a test NetCDF filename
-        path = os.path.abspath(
-            "../data/nc/core-1Hz_safire-piper-aztec_20110625_r0_az110014.nc")
-        self.netcdf = netcdf_geo.NetCDF(path)
+        self.path = "/path/to/some/file"
 
-    def test_get_netcdf_var_from_regex(self):
-        pass
+    def test_nc_var_from_regex(self):
+        # Uppercase
+        with NetCDF(self.path) as ncdf:
+            ncdf.variables = ["test", "TIME"]
+            assert ncdf._nc_var_from_regex("time", ncdf,
+                                           re.IGNORECASE) == "TIME"
+
+        # Lowercase
+        with NetCDF(self.path) as ncdf:
+            ncdf.variables = ["test", "more test",
+                              "time", "teeeeeeest"]
+            assert ncdf._nc_var_from_regex("time", ncdf,
+                                           re.IGNORECASE) == "time"
+
+        # Error
+        with NetCDF(self.path) as ncdf:
+            ncdf.variables = ["brian", "spam", "error"]
+            assert ncdf._nc_var_from_regex("time", ncdf,
+                                           re.IGNORECASE) is None
 
     def test_nc_var_from_list(self):
-        pass
+        with NetCDF(self.path) as ncdf:
+            possible_vars = ["pressure", "PRESSURE"]
+            ncdf.variables = ["pressure", "something else"]
+            assert ncdf._nc_var_from_list(possible_vars, ncdf) == "pressure"
+
+        with NetCDF(self.path) as ncdf:
+            possible_vars = ["altitude", "cheese", "ni"]
+            ncdf.variables = ["height", "angular momentum"]
+            self.assertRaises(AttributeError,
+                ncdf._nc_var_from_list, possible_vars, ncdf)
+
+    def test_get_time_data(self):
+        param = {
+            "name": "Brian"
+        }
+
+        mock = Mock(return_value=param)
+        #with NetCDF(self.path) as ncdf:
+        #    ncdf.get_time_data(mock)
+
+    def test_get_parameters(self):
+        param = {
+            "name": "Brian"
+        }
+
+        mock = Mock(return_value=param)
+        #with NetCDF(self.path) as ncdf:
+        #    ncdf.get_time_data(mock)
 
     def test_time_from_num_format(self):
         pass
 
     def test_time_from_str_format(self):
-        pass
-
-    def test_get_time_data(self):
         pass
 
     def test_get_geospatial(self):
