133530f7a040de636526fe23433c1d5729f0b2ff
Charles Newey <charlie@assemblyco.de>
2014-09-18 14:55:27 +0100

    Improve basic tests in test_netcdf_geo

--- python/src/test/test_netcdf_geo.py
+++ python/src/test/test_netcdf_geo.py
@@ -4,10 +4,9 @@ Test module for eufar.netcdf_geo
 
 import re
 import unittest
-from mock import Mock
 
 from eufar.netcdf_geo import NetCDF
-from test import netCDF4
+from eufar.metadata.product import Parameter
 
 class TestNetCDF(unittest.TestCase):
     """
@@ -17,20 +16,17 @@ class TestNetCDF(unittest.TestCase):
         self.path = "/path/to/some/file"
 
     def test_nc_var_from_regex(self):
-        # Uppercase
         with NetCDF(self.path) as ncdf:
             ncdf.variables = ["test", "TIME"]
             assert ncdf._nc_var_from_regex("time", ncdf,
                                            re.IGNORECASE) == "TIME"
 
-        # Lowercase
         with NetCDF(self.path) as ncdf:
             ncdf.variables = ["test", "more test",
                               "time", "teeeeeeest"]
             assert ncdf._nc_var_from_regex("time", ncdf,
                                            re.IGNORECASE) == "time"
 
-        # Error
         with NetCDF(self.path) as ncdf:
             ncdf.variables = ["brian", "spam", "error"]
             assert ncdf._nc_var_from_regex("time", ncdf,
@@ -48,28 +44,27 @@ class TestNetCDF(unittest.TestCase):
             self.assertRaises(AttributeError,
                 ncdf._nc_var_from_list, possible_vars, ncdf)
 
-    def test_get_time_data(self):
-        param = {
-            "name": "Brian"
-        }
-
-        mock = Mock(return_value=param)
-        #with NetCDF(self.path) as ncdf:
-        #    ncdf.get_time_data(mock)
-
     def test_get_parameters(self):
-        param = {
-            "name": "Brian"
-        }
+        class Mock(object):
+            __dict__ = {"thing": "Brian",
+                        "something": "some other thing"}
 
-        mock = Mock(return_value=param)
-        #with NetCDF(self.path) as ncdf:
-        #    ncdf.get_time_data(mock)
+        item = Mock()
+        param = {"name": item}
+        with NetCDF(self.path) as ncdf:
+            ncdf.variables = param
+            assert isinstance(ncdf.get_parameters(ncdf), list)
+            assert isinstance(ncdf.get_parameters(ncdf)[0], Parameter)
 
     def test_time_from_num_format(self):
         pass
 
     def test_time_from_str_format(self):
+        pass
+
+    def test_get_time_data(self):
+        #with NetCDF(self.path) as ncdf:
+        #    ncdf.get_time_data(ncdf)
         pass
 
     def test_get_geospatial(self):
