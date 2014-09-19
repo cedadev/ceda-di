69f48df362ffe3ae879521d33b902fe180f1cdd1
Charles Newey <charlie@assemblyco.de>
2014-09-19 12:30:19 +0100

    Add test for get_temporal

--- python/src/test/test_hdf4_geo.py
+++ python/src/test/test_hdf4_geo.py
@@ -10,13 +10,14 @@ from eufar.hdf4_geo import HDF4
 class MockAttachedVData(object):
     def __init__(self, read_data):
         self.count = 0
-        self.read_data = read_data
+        self.read_data = [read_data]
 
     def read(self):
         self.count += 1
-        if self.count > 2:
+        try:
+            return [self.read_data[0][self.count - 1]]
+        except IndexError:
             raise HDF4Error()
-        return self.read_data
 
     def detach(self):
         pass
@@ -25,22 +26,47 @@ class MockAttachedVData(object):
 class MockVData(object):
     def __init__(self, read_data):
         self.read_data = read_data
+
     def find(self, k):
-        return "Test"
+        return k
 
     def attach(self, k):
-        return MockAttachedVData(self.read_data)
+        return MockAttachedVData(self.read_data[k])
 
 
 class TestHDF4(unittest.TestCase):
-    # Mock an HDF4 VData object that supports reading
     def setUp(self):
         self.path = "/non/existent/path"
 
     def test_get_coords(self):
+        # Mock an HDF4 VData object that supports reading
         hdf = HDF4(self.path)
-        coords = hdf._get_coords(MockVData([[12345678]]), self.path)
+        m = MockVData({
+            "NVlat2": [[12345678]],
+            "NVlng2": [[12345678]]
+        })
+
+        coords = hdf._get_coords(m, self.path)
         assert coords == {
-            "lat": [1.2345678, 1.2345678],
-            "lon": [1.2345678, 1.2345678]
+            "lat": [1.2345678],
+            "lon": [1.2345678]
         }
+
+    def test_get_temporal(self):
+        hdf = HDF4(self.path)
+
+        m = MockVData({
+            "MIdate": [[ord(x)] for x in "19/07/10"],
+            "MIstime": [["100419"]],
+            "MIetime": [["110101"]],
+        })
+
+        timestamps = hdf._get_temporal(m, self.path)
+        print timestamps
+        assert timestamps == {
+            "start_time": "2010-07-19T10:04:19",
+            "end_time": "2010-07-19T11:01:01",
+        }
+
+    def get_geospatial(self):
+        pass
