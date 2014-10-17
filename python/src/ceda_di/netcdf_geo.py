4c3dfbeb41c6ce5abad65fcf86a626167b75d303
Charles Newey <charlie@assemblyco.de>
2014-10-17 15:09:57 +0100

    Implement metadata extraction for NCAR-RAF

--- python/src/ceda_di/netcdf_geo.py
+++ python/src/ceda_di/netcdf_geo.py
@@ -42,6 +42,8 @@ class NetCDF_Base(_geospatial):
     @staticmethod
     def params(ncdf):
         """
+        Construct list of Parameters based on variables in NetCDF file.
+        :param Dataset ncdf: Reference to an opened netcdf4.Dataset object
         :return list: List of metadata.product.Parameter objects
         """
         params = []
@@ -54,7 +56,8 @@ class NetCDF_Base(_geospatial):
     @staticmethod
     def geospatial(ncdf, lat_name, lon_name):
         """
-        Returns a dict containing normalised and sanitised lat/lons.
+        Return a dict containing lat/lons from NetCDF file.
+        :param Dataset ncdf: Reference to an opened netcdf4.Dataset object
         :param lat_name: Name of parameter containing latitude values
         :param lon_name: Name of parameter containing longitude values
         :return geospatial: Geospatial information as dict.
@@ -68,6 +71,11 @@ class NetCDF_Base(_geospatial):
 
     @staticmethod
     def temporal(ncdf, time_name):
+        """
+        Extract time values from Dataset using the variable name provided.
+        :param Dataset ncdf: Reference to an opened netcdf4.Dataset object
+        :param str time_name: Name of the time parameter
+        """
         times = list(netCDF4.num2date(list(ncdf.variables[time_name]),
                                       ncdf.variables[time_name].units))
         return {
@@ -75,15 +83,43 @@ class NetCDF_Base(_geospatial):
             "end_time": times[-1].isoformat()
         }
 
+    @staticmethod
+    def find_var_by_standard_name(ncdf, standard_name):
+        """
+        Find a variable reference searching by CF standard name.
+        :param Dataset ncdf: Reference to an opened netCDF4.Dataset object
+        :param str standard_name: The CF standard name to search for
+        """
+        for key, value in ncdf.variables.iteritems():
+            try:
+                if value.standard_name.lower() == standard_name.lower():
+                    return key
+            except AttributeError:
+                continue
+
+        logger = logging.getLogger(__name__)
+        logger.info("Could not find standard name variable: \"%s\"",
+                     (standard_name))
+
+    @staticmethod
+    def find_var_by_regex(ncdf, regex):
+        """
+        Find a variable reference searching by regular expression.
+        :param Dataset ncdf: Reference to an opened netCDF4.Dataset object
+        :param re regex: Regular expression to match with variable name
+        """
+        for key in ncdf.variables.keys():
+            if re.match(regex, key, flags=re.IGNORECASE):
+                return key
+
+        logger = logging.getLogger(__name__)
+        logger.info("Could not find variable by regex: \"%s\"", regex)
 
 class NetCDF_CF(_geospatial):
     """
-    CF-compliant NetCDF metadata extraction class.
+    Metadata extraction class for CF-compliant NetCDF files.
     """
     def __init__(self, fpath, convention):
-        """
-        :param str fpath: Path to NetCDF file
-        """
         self.fpath = fpath
         self.logger = logging.getLogger(__name__)
         self.convention = convention
@@ -98,14 +134,8 @@ class NetCDF_CF(_geospatial):
 
     def get_geospatial(self):
         with netCDF4.Dataset(self.fpath) as ncdf:
-            for key, value in ncdf.variables.iteritems():
-                try:
-                    if value.standard_name == "latitude":
-                        lat_name = key
-                    elif value.standard_name == "longitude":
-                        lon_name = key
-                except AttributeError:
-                    continue
+            lat_name = NetCDF_Base.find_var_by_standard_name(ncdf, "latitude")
+            lon_name = NetCDF_Base.find_var_by_standard_name(ncdf, "longitude")
 
             if lat_name and lon_name:
                 return NetCDF_Base.geospatial(ncdf, lat_name, lon_name)
@@ -118,21 +148,17 @@ class NetCDF_CF(_geospatial):
         Return a metadata.product.Properties object populated with metadata.
         :return properties: Properties object populated with metadata
         """
-
-        temporal = self.get_temporal()
-        filesystem = self.get_filesystem(self.fpath)
-        spatial = self.get_geospatial()
         data_format = {"format": ("NetCDF/%s" % self.convention)}
-        parameters = self.get_parameters()
-
-        return product.Properties(temporal=temporal,
-                                  filesystem=filesystem,
-                                  spatial=spatial,
+        return product.Properties(temporal=self.get_temporal(),
+                                  filesystem=self.get_filesystem(self.fpath),
+                                  spatial=self.get_geospatial(),
                                   data_format=data_format,
-                                  parameters=parameters)
+                                  parameters=self.get_parameters())
+
+
 class NetCDF_RAF(_geospatial):
     """
-    :param str fpath: Path to NetCDF file
+    Metadata extraction class for NCAR-RAF-compliant NetCDF.
     """
     def __init__(self, fpath, convention):
         self.fpath = fpath
@@ -141,24 +167,51 @@ class NetCDF_RAF(_geospatial):
 
     def get_temporal(self):
         with netCDF4.Dataset(self.fpath) as ncdf:
-            for key, value in ncdf.variables.iteritems():
-                try:
-                    if value.standard_name.lower == "time":
-                        time_name = key
-                except AttributeError:
-                    self.logger.error("Could not find time variable: %s" %
-                                      self.fpath)
-
+            time_name = NetCDF_Base.find_var_by_standard_name(ncdf, "time")
             return NetCDF_Base.temporal(ncdf, time_name)
+
+    def get_parameters(self):
+        with netCDF4.Dataset(self.fpath) as ncdf:
+            return NetCDF_Base.params(ncdf)
+
+    def get_geospatial(self):
+        try:
+            # Try finding corrected latitude and longitude
+            with netCDF4.Dataset(self.fpath) as ncdf:
+                return NetCDF_Base.geospatial(ncdf, "LATC", "LONC")
+        except AttributeError:
+            lat_name = NetCDF_Base.find_var_by_standard_name(ncdf, "latitude")
+            lon_name = NetCDF_Base.find_var_by_standard_name(ncdf, "longitude")
+
+            if lat_name and lon_name:
+                return NetCDF_Base.geospatial(ncdf, "LATC", "LONC")
+            else:
+                self.logger.info("Couldn't find lat/lon variables: %s" %
+                                 self.fpath)
+
+    def get_properties(self):
+        """
+        Return a metadata.product.Properties object populated with metadata.
+        :return properties: Properties object populated with metadata
+        """
+        data_format = {"format": ("NetCDF/%s" % self.convention)}
+        return product.Properties(temporal=self.get_temporal(),
+                                  filesystem=self.get_filesystem(self.fpath),
+                                  spatial=self.get_geospatial(),
+                                  data_format=data_format,
+                                  parameters=self.get_parameters())
+
 
 class NetCDF_Unknown(_geospatial):
     """
     A wrapper to extract NetCDF metadata that is stored using an unknown
-    metadata convention.
-    :param str fpath: Path to NetCDF file
+    metadata convention, or no known associated metadata convention.
     """
     def __init__(self, fpath):
+        self.fpath = fpath
         self.logger = logging.getLogger(__name__)
-        self.logger.info("File \"%s\" has no associated metadata convention." %
-                         fpath)
-        pass
+        self.logger.info("No metadata convention: \"%s\"" % fpath)
+
+    def get_geospatial(self):
+        raise TypeError("Cannot extract metadata from \"%s\"" +  # Continued
+                        "(nonconformant format" % self.fpath)
