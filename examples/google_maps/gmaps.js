995f80d714a59a0facee80d6894f200a5e85daf1
Charles Newey <charlie@assemblyco.de>
2014-10-30 17:55:52 +0000

    In-progress refactoring of gmaps.js

--- examples/google_maps/gmaps.js
+++ examples/google_maps/gmaps.js
@@ -1,187 +1,57 @@
 /*jslint browser: true, devel: true, sloppy: true*/
-/*global google, $ */
+/*global google, $*/
 
-/*---------------------------- Setup ----------------------------*/
-// Constants
-var es_url = "http://fatcat-test.jc.rl.ac.uk:9200/badc/eufar/_search";
-var wps_url = "http://ceda-wps2.badc.rl.ac.uk:8080/submit/form?proc_id=PlotTimeSeries&FilePath=";
-var geocoder = new google.maps.Geocoder();
-var map = new google.maps.Map(
-    document.getElementById('map'),
-    {
-        mapTypeId: google.maps.MapTypeId.TERRAIN,
-        zoom: 4
-    }
-);
+// Window constants
+var track_colours = ["#4D4D4D", "#5DA5DA", "#FAA43A",
+                "#60BD68", "#F17CB0", "#B2912F",
+                "#B276B2", "#DECF3F", "#F15854"];
 
-// Colour palette from here: http://bit.ly/1wLGsBG
-var path_colours = ["#4D4D4D", "#5DA5DA", "#FAA43A",
-                    "#60BD68", "#F17CB0", "#B2912F",
-                    "#B276B2", "#DECF3F", "#F15854"]
-
-// Global arrays
-window.polygons = []; // Array of polygon shapes drawn from ES requests
-window.info_windows = []; // Array of InfoWindows (one for each polygon)
-
-/*---------------------------- Functions ----------------------------*/
+// -----------------------------String.hashCode--------------------------------
 String.prototype.hashCode = function () {
     // Please see: http://bit.ly/1dSyf18 for original
-    var hash = 0;
-    if (this.length == 0) return hash;
+    var i, c, hash;
+
+    hash = 0;
+    if (this.length === 0) {
+        return hash;
+    }
+
     for (i = 0; i < this.length; i++) {
         c = this.charCodeAt(i);
         hash = ((hash << 5) - hash) + c;
     }
+
     return hash;
-}
+};
 
-function location_search() {
-    var loc = $("#location").val();
-    if (loc === "") {
-        alert("Please enter a value into the 'Location' box.");
-    } else {
-        geocoder.geocode({
-            "address": loc
-        },
-            function (results, status) {
-                if (status === "OK") {
-                    var new_centre = results[0].geometry.location;
-                    // Centre map on new location
-                    map.panTo(new_centre);
-                } else {
-                    alert("Could not find '" + loc + "'");
-                }
-            }
-        );
-    }
-}
+// -------------------------------ElasticSearch--------------------------------
+var es_url = "http://fatcat-test.jc.rl.ac.uk:9200/badc/eufar/_search";
+function request_from_filters(full_text) {
+    var req;
 
-function get_text_filters() {
-    full_text_filter = $("#ftext").val();
-    if (full_text_filter.length > 0) {
-        return {
+    if (full_text.length > 0) {
+        req = {
             "term": {
-                "_all": full_text_filter
+                "_all": full_text
             }
         };
+
+        return req;
     }
 }
 
-// Construct a google.maps.Polygon object from a bounding box
-function construct_polygon(bbox, hit_id) {
-    var vertices, polygon;
+function create_elasticsearch_request(gmaps_corners, full_text) {
+    var tmp_ne, tmp_sw, nw, se, request, tf;
 
-    vertices = [];
-    for (i = 0; i < bbox.length; i += 1) {
-        vertices.push(new google.maps.LatLng(bbox[i][1], bbox[i][0]));
-    }
+    tmp_ne = gmaps_corners.getNorthEast();
+    tmp_sw = gmaps_corners.getSouthWest();
+    nw = [tmp_sw.lng().toString(), tmp_ne.lat().toString()];
+    se = [tmp_ne.lng().toString(), tmp_sw.lat().toString()];
 
-    // Pick a colour for the line based on hash value of _id
-    index = (hit_id.hashCode() % path_colours.length);
-    if (index < 0)  index = -index;
-    lineColor = path_colours[index];
-
-    polygon = new google.maps.Polyline({
-        path: vertices,
-        geodesic: true,
-        strokeColor: lineColor,
-        strokeWeight: 5,
-        strokeOpacity: 0.8
-    });
-
-    return polygon;
-}
-
-// Construct an InfoWindow from a search hit, containing useful info
-function construct_info_window(hit) {
-    var content, info;
-
-    content = "<section><p><strong>Filename: </strong>" + hit.file.filename + "</p>";
-
-    if (hit.temporal) {
-        content += "<p><strong>Start Time: </strong>" +
-                    hit.temporal.start_time + "</p>" +
-                    "<p><strong>End Time: </strong>" +
-                    hit.temporal.end_time + "</p>";
-    }
-
-    if (hit.misc.flight_num) {
-        content += "<p><strong>Flight Num: </strong>\"" +
-                    hit.misc.flight_num + "\"</p>"
-    }
-
-    if (hit.misc.organisation) {
-        content += "<p><strong>Organisation: </strong>\"" +
-                    hit.misc.organisation + "\"</p>"
-    }
-
-    content += "<p><a href=\"http://badc.nerc.ac.uk/browse" +
-                   hit.file.path + "\">Get data</a></p>";
-
-    if (hit.data_format.format.search("RAF") > 0) {
-        content += "<p><a href=\"" + wps_url +
-            hit.file.path + "\" target=\"_blank\">Plot time-series</a></p>";
-    }
-
-    content += "</section>";
-
-    info = new google.maps.InfoWindow({content: content});
-    return info;
-}
-
-function draw_polygons(hits) {
-    var i, j, polygon, hit, bbox, iw;
-
-    for (i = 0; i < hits.length; i += 1) {
-        hit = hits[i]._source;
-        hit_id = hits[i]._id;
-        bbox = hit.spatial.geometries.summary.coordinates;
-
-        // Construct and display polygon
-        polygon = construct_polygon(bbox, hit_id);
-        polygon.setMap(map);
-        window.polygons.push(polygon);
-
-        // Add info window
-        iw = construct_info_window(hits[i]._source);
-        window.info_windows.push(iw);
-    }
-
-    // Add a listener function to polygon that opens a new
-    // InfoWindow on top of the polygon that was clicked
-    // (closes all other InfoWindows first)
-    for (i = 0; i < hits.length; i += 1) {
-        google.maps.event.addListener(window.polygons[i], 'click',
-            (function (i, e) {
-                return function (e) {
-                    for (j = 0; j < window.info_windows.length; j += 1) {
-                        window.info_windows[j].close();
-                    }
-                    window.info_windows[i].open(map, null);
-                    window.info_windows[i].setPosition(e.latLng);
-                };
-            }
-        (i)));
-    }
-}
-
-// Creates an ES geo_shape filter query based on a bounding box
-function create_es_request(bbox, offset) {
-    var temp_ne, temp_sw, nw, se, request, i;
-
-    temp_ne = bbox.getNorthEast();
-    temp_sw = bbox.getSouthWest();
-
-    // Build search extent using NW/SE and lng/lat format
-    nw = [temp_sw.lng().toString(),
-          temp_ne.lat().toString()];
-    se = [temp_ne.lng().toString(),
-          temp_sw.lat().toString()];
-
-    // Construct request for ElasticSearch
+    // ElasticSearch request
     request = {
-        "_source": {
-            "include": [
+        _source: {
+            include: [
                 "data_format.format",
                 "file.filename",
                 "file.path",
@@ -190,15 +60,15 @@ function create_es_request(bbox, offset) {
                 "temporal"
             ]
         },
-        "filter": {
-            "and": {
-                "must": [
+        filter: {
+            and: {
+                must: [
                     {
-                        "geo_shape": {
-                            "bbox": {
-                                "shape": {
-                                    "type": "envelope",
-                                    "coordinates": [nw, se]
+                        geo_shape: {
+                            bbox: {
+                                shape: {
+                                    type: "envelope",
+                                    coordinates: [nw, se]
                                 }
                             }
                         }
@@ -206,140 +76,202 @@ function create_es_request(bbox, offset) {
                 ]
             }
         },
-        "size": 30
+        size: 30,
     };
 
-    // Add any extra user-defined filters
-    tf = get_text_filters();
+    // Add extra filters from free-text search box
+    tf = request_from_filters(full_text);
     if (tf) {
-        request.filter.and.must.unshift(tf);
+        request.filter.and.must.push(tf);
     }
 
     return request;
 }
 
-// Search ES for data (and display received data asynchronously)
-function search_es_bbox(bbox) {
-    var xhr, request, response, hits;
+function send_elasticsearch_request(gmap, full_text) {
+    var xhr, request, response;
 
-    // Create and send request
+    request = create_elasticsearch_request(gmap.getBounds(), full_text);
+
+    // Construct and send XMLHttpRequest
     xhr = new XMLHttpRequest();
     xhr.open("POST", es_url, true);
-
-    request = create_es_request(bbox);
     xhr.send(JSON.stringify(request));
-
-    // Handle the response
     xhr.onload = function (e) {
         if (xhr.readyState === 4) {
             response = JSON.parse(xhr.responseText);
             if (response.hits) {
+                console.log(JSON.stringify(response));
                 $("#resptime").html(response.took);
-
-                // Update "number of hits" field in sidebar
                 $("#numresults").html(response.hits.total);
 
-                // Loop through each hit, drawing as necessary
-                hits = response.hits.hits;
-                draw_polygons(hits);
+                draw_flight_tracks(response.hits.hits);
             }
         }
     };
 }
 
-function cleanup() {
-    var p, i;
+// -----------------------------------Map--------------------------------------
+var flight_tracks = [];
+var info_windows = [];
 
-    // Clean up old polygons and info windows
-    for (p = 0; p < window.polygons.length; p += 1) {
-        window.polygons[p].setMap(null);
+function centre_map(gmap, geocoder, loc) {
+    if (loc !== "") {
+        geocoder.geocode({
+            address: loc
+        },
+        function (results, status) {
+            if (status === "OK") {
+                gmap.panTo(results[0].geometry.location);
+            } else {
+                alert("Could not find \"" + loc + "\"");
+            }
+        });
     }
-
-    for (i = 0; i < window.info_windows.length; i += 1) {
-        window.info_windows[i].close();
-    }
-
-    window.polygons = [];
-    window.info_windows = [];
 }
 
-// Redraw the map (inc. polygons, etc)
-function redraw_map() {
-    var bounds;
+function create_info_window(hit) {
+    var content, info;
 
+    content = "<section><p><strong>Filename: </strong>" + hit.file.filename + "</p>";
+    if (hit.temporal) {
+        content += "<p><strong>Start Time: </strong>" + hit.temporal.start_time + "</p>" +
+                    "<p><strong>End Time: </strong>" + hit.temporal.end_time + "</p>";
+    }
+
+    if (hit.misc.flight_num) {
+        content += "<p><strong>Flight Num: </strong>\"" + hit.misc.flight_num + "\"</p>";
+    }
+
+    if (hit.misc.organisation) {
+        content += "<p><strong>Organisation: </strong>\"" + hit.misc.organisation + "\"</p>";
+    }
+
+    content += "<p><a href=\"http://badc.nerc.ac.uk/browse" + hit.file.path + "\">Get data</a></p>";
+    if (hit.data_format.format.search("RAF") > 0) {
+        content += "<p><a href=\"" + wps_url + hit.file.path + "\" target=\"_blank\">Plot time-series</a></p>";
+    }
+
+    content += "</section>";
+    info = new google.maps.InfoWindow({content: content});
+    return info;
+}
+
+function draw_flight_tracks(hits) {
+    var i, j, track, hit, iw;
+
+    for (i = 0; i < hits.length; i+= 1) {
+        hit = hits[i];
+
+        // Construct flight track (flipping coordinates in the process)
+        coords = hit._source.spatial.geometries.summary.coordinates;
+        for (j = 0; j < coords.length; j += 1) {
+            coords[i] = [coords[i][1], coords[i][0]];
+        }
+
+        colour_index = (hit._id.hashCode() % track_colours.length);
+        if (colour_index < 0) {
+            colour_index = -colour_index;
+        }
+
+        track = new google.maps.Polyline({
+            path: coords,
+            geodesic: true,
+            strokeColor: track_colours[colour_index],
+            strokeWeight: 5,
+            strokeOpacity: 0.8
+        });
+
+        // Construct info window
+        info_window = create_info_window(hit);
+
+        // Add to lists
+        flight_tracks.append(track);
+        info_windows.append(info_window);
+
+        // Add a listener function to track that opens an InfoWindow
+        // (closes all other InfoWindows first)
+        //
+        /*
+        google.maps.event.addListener(track, 'click',
+            (function (i, e) {
+                return function (e) {
+                    for (j = 0; j < info_windows.length; j += 1) {
+                        info_windows[j].close();
+                    }
+
+                    info_windows[i].open(map, null);
+                    info_windows[i].setPosition(e.latLng);
+                };
+            }
+        (i)));
+        */
+    }
+}
+
+function cleanup() {
+    var i, j;
+
+    for (i = 0; i < flight_tracks.length; i += 1) {
+        flight_tracks[i].setMap(null);
+    }
+    flight_tracks = [];
+
+    for (i = 0; i < info_windows.length; i += 1) {
+        info_windows[i].close();
+    }
+    info_windows = [];
+}
+
+function redraw_map(gmap) {
     cleanup();
 
-    // Make ES request
-    bounds = map.getBounds();
-    search_es_bbox(bounds);
+    // Draw flight tracks
+    full_text = $("#ftext").val();
+    send_elasticsearch_request(gmap, full_text);
 
-    // Rate-limit requests to ES to 1 per second
     window.setTimeout(function () {
         add_bounds_changed_listener(map);
     }, 500);
 }
 
-// Add a listener to make an ES data request when the map bounds change
-function add_bounds_changed_listener(map) {
-    google.maps.event.addListenerOnce(map, 'bounds_changed', function () {
-        redraw_map();
+function add_bounds_changed_listener(gmap) {
+    google.maps.event.addListenerOnce(gmap, "bounds_changed", function () {
+        redraw_map(gmap);
     });
 }
 
-// Start the main "bounds_changed" listener loop
+// ------------------------------window.onload---------------------------------
 window.onload = function () {
-    // Draw histogram
-    var resp = request_histogram();
+    var geocoder, map, trackColours;
 
-    // Add listener to update mouse position
-    // see: http://bit.ly/1zAfter
+
+    // Google Maps geocoder and map object
+    geocoder = new google.maps.Geocoder();
+    map = new google.maps.Map(
+        document.getElementById("map"),
+        {
+            mapTypeId: google.maps.MapTypeId.TERRAIN,
+            zoom: 4
+        }
+    );
+
+    centre_map(map, geocoder, "Lake Balaton, Hungary");
     google.maps.event.addListener(map, 'mousemove', function(event) {
+        // Add listener to update mouse position
+        // see: http://bit.ly/1zAfter
         lat = event.latLng.lat().toFixed(4);
         lon = event.latLng.lng().toFixed(4);
 		$("#mouse").html(lat + ', ' + lon);
 	});
 
-
-    // Centre the map on Hungary initially
-    geocoder.geocode(
-        {
-            "address": "Lake Balaton, Hungary"
-        },
-        function (results, status) {
-            if (status === "OK") {
-                map.setCenter(results[0].geometry.location);
-            }
-        }
-    );
-
-    // Set up location 'search' button
+    // Set up buttons
     $("#location_search").click(
         function () {
-            location_search();
+            centre_map(map, geocoder, $("#location").val());
         }
     );
 
-    // Clears all input values
-    $("#clearfil").click(
-        function () {
-            $("#ftext").val("");
-            $("#start_time").val("");
-            $("#end_time").val("");
-
-            cleanup();
-            redraw_map();
-        }
-    );
-
-    // Constructs query filters from input values
-    $("#applyfil").click(
-        function () {
-            cleanup();
-            redraw_map();
-        }
-    );
-
-    // Override location form submission on 'enter'
     $("#location").keypress(
         function (e) {
             var charcode = e.charCode || e.keyCode || e.which;
@@ -347,6 +279,15 @@ window.onload = function () {
                 location_search();
                 return false;
             }
+        }
+    );
+
+    $("#clearfil").click(
+        function () {
+            $("#ftext").val("");
+
+            cleanup();
+            redraw_map(map);
         }
     );
 
