c628915832757aeff981b0ce73cdaf7d5b85a063
Charles Newey <charlie@assemblyco.de>
2015-02-18 17:27:44 +0000

    Tests for changes to GeoJSONGenerator

--- python/src/test/test_product.py
+++ python/src/test/test_product.py
@@ -120,3 +120,152 @@ class TestGeoJSONGenerator(unittest.TestCase):
         for result, expected in zip(geojson["geometries"]["display"]["coordinates"], [(0, 3.0), (100, 6.5)]):
             assert_that(result[0], close_to(expected[0], 0.01), "lat" )
             assert_that(result[1], close_to(expected[1], 0.01), "lon")
+
+    def test_GIVEN_more_than_one_entry_and_no_type_THEN_geojson_is_track_type(self):
+        latitudes = [3, 5, 1, 6]
+        longitudes = [0, 120,  -120, 100]
+
+        gen = GeoJSONGenerator(latitudes, longitudes)
+        geojson = gen.get_elasticsearch_geojson()
+        expected_geojson = {
+            "geometries": {
+                "search": {
+                    "type": "LineString",
+                    "coordinates": [
+                        (0, 3),
+                        (120, 5),
+                        (-120, 1),
+                        (100, 6)
+                    ]
+                },
+                "display": {
+                    "type": "LineString",
+                    "coordinates": [
+                        (0, 3),
+                        (120, 5),
+                        (-120, 1),
+                        (100, 6)
+                    ]
+                }
+            }
+        }
+
+        assert_that(geojson, is_(expected_geojson))
+
+    def test_GIVEN_coordinates_with_point_type_THEN_geojson_is_point_type(self):
+        latitudes = [3, 5, 1, 6]
+        longitudes = [0, 120,  -120, 100]
+
+        gen = GeoJSONGenerator(latitudes, longitudes, shape_type="point")
+        geojson = gen.get_elasticsearch_geojson()
+
+        # Remember that if a "point" type annotation is supplied, then it's
+        # only the first pair of coordinates that are used.
+        expected_geojson = {
+            "geometries": {
+                "search": {
+                    "type": "Point",
+                    "coordinates": [0, 3]
+                },
+                "display": {
+                    "type": "Point",
+                    "coordinates": [0, 3]
+                }
+            }
+        }
+
+        assert_that(geojson, is_(expected_geojson))
+
+    def test_GIVEN_coordinates_with_swath_type_THEN_geojson_is_swath_type(self):
+        # This is a little hard to test without the codelooking horrific,
+        # so you'll have to take my word for it that this is supposed to
+        # generate three bounding boxes that cross Ireland.
+        #
+        # You can go to "http://geojsonlint.com" and paste in the GeoJSON in
+        # "expected_search_geojson" to verify.
+        longitudes = [
+            -10.854492, -9.711914, -7.734375, -5.537109
+        ]
+
+        latitudes = [
+            49.037868, 52.106505, 54.622978, 56.632064
+        ]
+
+        gen = GeoJSONGenerator(latitudes, longitudes, shape_type="swath")
+        geojson = gen.get_elasticsearch_geojson()
+        expected_search_geojson = {
+            "type": "MultiPolygon",
+            "coordinates": [
+                [
+                    [
+                        [-9.711914, 52.106505],
+                        [-10.854492, 52.106505],
+                        [-10.854492, 49.037868],
+                        [-9.711914, 49.037868],
+                        [-9.711914, 52.106505]
+                    ]
+                ],
+                [
+                    [
+                        [-7.734375, 54.622978],
+                        [-9.711914, 54.622978],
+                        [-9.711914, 52.106505],
+                        [-7.734375, 52.106505],
+                        [-7.734375, 54.622978]
+                    ]
+                ],
+                [
+                    [
+                        [-5.537109, 56.632064],
+                        [-7.734375, 56.632064],
+                        [-7.734375, 54.622978],
+                        [-5.537109, 54.622978],
+                        [-5.537109, 56.632064]
+                    ]
+                ]
+            ]
+        }
+
+        expected_display_geojson = {
+            "type": "LineString",
+            "coordinates": zip(longitudes, latitudes)
+        }
+
+        assert_that(geojson["geometries"]["search"], is_(expected_search_geojson))
+        assert_that(geojson["geometries"]["display"], is_(expected_display_geojson))
+
+    def test_GIVEN_coordinates_with_track_type_THEN_geojson_is_track_type(self):
+        latitudes = [3, 5, 1, 6]
+        longitudes = [0, 120,  -120, 100]
+
+        gen = GeoJSONGenerator(latitudes, longitudes, shape_type="track")
+        geojson = gen.get_elasticsearch_geojson()
+        expected_geojson = {
+            "geometries": {
+                "search": {
+                    "type": "LineString",
+                    "coordinates": [
+                        (0, 3),
+                        (120, 5),
+                        (-120, 1),
+                        (100, 6)
+                    ]
+                },
+                "display": {
+                    "type": "LineString",
+                    "coordinates": [
+                        (0, 3),
+                        (120, 5),
+                        (-120, 1),
+                        (100, 6)
+                    ]
+                }
+            }
+        }
+
+    def test_GIVEN_two_differently_sized_coordinate_lists_THEN_num_points_returns_correct_value(self):
+        latitudes = [3, 5, 1]  # shorter list
+        longitudes = [0, 120,  -120, 100]
+
+        gen = GeoJSONGenerator(latitudes, longitudes)
+        assert_that(gen._num_points(longitudes, latitudes), is_(3))
