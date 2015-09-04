a2ff57a6184df69f4b4ece74a7ddf2d8fcc46c46
Kleanthis Tsaousis <kleanthis@mist.badc.rl.ac.uk>
2015-09-04 17:12:56 +0100

    Initial changes for issue #23217

--- python/src/ceda_di/util/util.py
+++ python/src/ceda_di/util/util.py
@@ -101,3 +101,85 @@ def find_dataset(file, dataset_id):
         return vars
     else :
         return vars[dataset_id]
+
+
+def get_number_of_running_lotus_tasks() :
+    
+    """
+    Returns the number of running jobs in lotus. 
+    Counting is based on the observation that if there are not any jobs running then the system
+    returns a single line of text otherwise returns a line with headers and a list of running processes.  
+    TODO : Add some regex matching in order to identify the output.     
+    """
+    
+    num_of_running_tasks = subprocess.check_output('bjobs', stderr=subprocess.STDOUT, shell=True).count("\n") 
+            
+    if num_of_running_tasks > 0 :
+        num_of_running_tasks = num_of_running_tasks   - 1
+                 
+    return num_of_running_tasks
+    
+    
+def run_tasks_in_lotus(task_list, max_number_of_tasks_to_submit):
+    
+    """
+    Submits the commands supplied in lotus making sure that
+    max_number_of_jobs is not exceeded. 
+    """
+        
+    init_wait_time = 15
+    wait_time = init_wait_time
+    dec = 1
+    iterations_counter = 0
+             
+        
+    print "Max number of jobs to submit in each step :" + str(max_number_of_tasks_to_submit) + ". "\
+        + "\nTotal number commands to run " + str(len(task_list)) + ". " 
+                    
+    print "==============================="
+      
+        
+    while len(task_list) > 0 :            
+        
+        #Find out if other jobs can be submitted.
+        num_of_running_tasks = get_number_of_running_lotus_tasks() 
+        num_of_tasks_to_submit = max_number_of_tasks_to_submit - num_of_running_tasks             
+        iterations_counter = iterations_counter + 1             
+           
+        print "Iteration :" + str(iterations_counter) + ". " 
+        print "Number of jobs running  :" + str(num_of_running_tasks) + ". "    
+        print "Number of jobs to submit in this step :" + str(num_of_tasks_to_submit) + ". "
+            
+        #Submit jobs according to availability.
+        for i in range(0, num_of_tasks_to_submit):
+            
+            if len(task_list) == 0 : 
+                break
+            
+            #Is there an extract op ?
+            task = task_list[0]
+            task_list.remove(task)
+                                            
+            command = "bsub " + task 
+            
+            
+            print str(i +1) + ". " +"Executng :" + command
+            subprocess.call(command, shell=True)
+            
+            
+        print "Number of tasks waiting to be submitted :" + str(len(task_list)) + ". "           
+                        
+        #Wait in case some process terminates. 
+        print "Waiting for :" + str(wait_time) + " secs."            
+        time.sleep(wait_time)
+                                        
+                              
+        #If nothing can be submitted wait again.
+        if num_of_jobs_to_submit == 0:
+            wait_time = wait_time - dec
+            if (wait_time == 0):
+                wait_time = init_wait_time
+        
+        print "==============================="   
+    
+    
