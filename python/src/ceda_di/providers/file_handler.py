a9a60c0637019170da096413ce384abedd4fa857
Kleanthis Tsaousis <kleanthis@mist.badc.rl.ac.uk>
2015-09-16 17:04:42 +0100

    Changes for issue #23214.

--- python/src/ceda_di/providers/file_handler.py
+++ python/src/ceda_di/providers/file_handler.py
@@ -10,47 +10,43 @@ class GENERIC:
     def __init__(self):      
         self.doc={}
                 
-    def get_properties(self, file_path=None):
+    def get_properties(self, file_path):
         """
          Scans the given file and returns information about its properties and data.
         :returns: A dict containing a summary information.
         """         
-        
-        #exclude tmp files.
-        if (file_path is None):
+                
+        if file_path is None:
             return None
-                
-        file_exists = True
-        is_simlink = True
-                        
-        #check if file exists  
-        is_simlink = os.path.islink(file_path)
-        file_exists = os.path.isfile(file_path)
-                
         
-        if(file_exists or is_simlink):
-            #if file exists.
-            self.doc["directory"] = os.path.dirname(file_path)
-            self.doc["format"] = "data file"
-            self.doc["md5"] = ""
+        #check if file still exists  
+        file_exists = os.path.isfile(file_path) 
         
-            filename = os.path.basename(file_path) #ntpath.basename(file_path)
-            self.doc["name"] = filename 
-            self.doc["autocomplete"] = filename
+        if not file_exists :
+            return None      
+                                               
+        is_symlink = os.path.islink(file_path)
         
+        #kltsa 16/09/2015 change for issue 23214 : symbolic link files will be ignored.
+        if is_symlink :
+            return None  
         
-            if not is_simlink:
-                self.size = os.path.getsize(file_path)
-                self.size_h = self.size/(1024*1024.0)
-                self.doc["size"] = self.size_h
-                self.doc["type"] = "file"              
-            else:                 
-                self.doc["size"] = 0
-                self.doc["type"] = "symlink" 
-         
-            return self.doc
-        else :
-            return None        
+        #This is a regular file.       
+        self.doc["directory"] = os.path.dirname(file_path)
+        self.doc["format"] = "data file"
+        self.doc["md5"] = ""
+        
+        filename = os.path.basename(file_path) #ntpath.basename(file_path)
+        self.doc["name"] = filename 
+        self.doc["autocomplete"] = filename
+        
+        self.size = os.path.getsize(file_path)
+        self.size_h = self.size/(1024*1024.0)
+        self.doc["size"] = self.size_h
+        self.doc["type"] = "file"              
+           
+        return self.doc
+        
                 
     def __enter__(self):
         return self
