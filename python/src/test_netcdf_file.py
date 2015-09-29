addb479bf4afe5aba6a0f2a2559d767e2df29e80
kltsa <kleanthis.tsaousis@stfc.ac.uk>
2015-09-30 01:07:44 +0300

    Initial changes for issue #23222.

--- python/src/test_netcdf_file.py
+++ python/src/test_netcdf_file.py
@@ -10,19 +10,27 @@ def main():
     
     #29/09/2015 Test code delete after done.
     
+    from elasticsearch import Elasticsearch
+    es = Elasticsearch()   
+    
     print "script started."  
          
     ob = NetCDFFile()
     
-    res = ob.phenomena("/home/kleanthis/Dev/test_data_files/hus_cfDay_MPI-ESM-LR_1pctCO2_r1i1p1_19750101-19751231.nc") 
+    doc = ob.phenomena("/home/kleanthis/Dev/test_data_files/hus_cfDay_MPI-ESM-LR_1pctCO2_r1i1p1_19750101-19751231.nc") 
     
-     
+    res = es.index(index="test-index", doc_type='test-type', id=1, body=doc)
+    
+    print(res['created'])
+    
+    
+    res = es.search(index="test-index", body={"query": {"match_all": {}}})
+    print("Got %d Hits:" % res['hits']['total'])
+    
+    for hit in res['hits']['hits']:
+        print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
             
     print "script ended" + str(res)
-    
-    #TODO : traverse the res list.
-    #       Create json or sent direclty to elastic search ?
-
 
 
 if __name__ == '__main__':
