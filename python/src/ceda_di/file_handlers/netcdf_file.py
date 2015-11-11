181ce80e9122c68aa8ff35a35c6536f3810144bd
Kleanthis Tsaousis <kleanthis@mist.badc.rl.ac.uk>
2015-11-11 17:43:00 +0000

    Changes for issue 23233.

--- python/src/ceda_di/file_handlers/netcdf_file.py
+++ python/src/ceda_di/file_handlers/netcdf_file.py
@@ -16,13 +16,99 @@ class   NetCdfFile(GenericFile):
     def get_handler_id(self):
         return self.handler_id
 
+    def clean_coordinate(self, coord):
+        """Return True if coordinate is valid."""
+        try:
+            # This filters out misconfigured "_FillValue" elements
+            if coord == 0.0:
+                return False
+
+            int(coord)  # If this fails, "coord" is not a number!
+
+            return True
+        except ValueError:
+            return False
+
+
+    def geospatial(self, ncdf, lat_name, lon_name):
+        """
+        Return a dict containing lat/lons from NetCDF file.
+
+        :param Dataset ncdf: Reference to an opened netcdf4.Dataset object
+        :param lat_name: Name of parameter containing latitude values
+        :param lon_name: Name of parameter containing longitude values
+        :returns: Geospatial information as dict.
+        """
+
+        # Filter out items that are equal to "masked"
+        lats = filter(self.clean_coordinate,
+                      ncdf.variables[lat_name][:].ravel())
+        lons = filter(self.clean_coordinate,
+                      ncdf.variables[lon_name][:].ravel())
+        return {
+            "type": "track",
+            "lat": lats,
+            "lon": lons
+        }
+
+
+    def find_var_by_standard_name(self, ncdf, fpath, standard_name):
+        """
+        Find a variable reference searching by CF standard name.
+
+        :param Dataset ncdf: Reference to an opened netCDF4.Dataset object
+        :param str standard_name: The CF standard name to search for
+        """
+        for key, value in ncdf.variables.iteritems():
+            try:
+                if value.standard_name.lower() == standard_name.lower():
+                    return key
+            except AttributeError:
+                continue
+
+        logger = logging.getLogger(__name__)
+        logger.error("Could not find standard name variable \"%s\": %s" %
+                     (standard_name, fpath))
+
+    #ok lets try something new.
+    def get_geospatial(self):
+        with netCDF4.Dataset(self.file_path) as ncdf:
+            lat_name = self.find_var_by_standard_name(ncdf, self.file_path, "latitude")
+            lon_name = self.find_var_by_standard_name(ncdf, self.file_path, "longitude")
+
+            if lat_name and lon_name:
+                return self.geospatial(ncdf, lat_name, lon_name)
+            else:
+                self.logger.error("Could not find lat/lon variables: %s" %
+                                  self.file_path)
+
+    def temporal(self, ncdf, time_name):
+        """
+        Extract time values from Dataset using the variable name provided.
+
+        :param Dataset ncdf: Reference to an opened netcdf4.Dataset object
+        :param str time_name: Name of the time parameter
+        """
+        times = list(netCDF4.num2date(list(ncdf.variables[time_name]),
+                                      ncdf.variables[time_name].units))
+        return {
+            "start_time": times[0].isoformat(),
+            "end_time": times[-1].isoformat()
+        }
+
+
+    def get_temporal(self):
+        with netCDF4.Dataset(self.file_path) as ncdf:
+            time_name = self.find_var_by_standard_name(ncdf, self.file_path, "time")
+            return self.temporal(ncdf, time_name)
+ 
+
     def phenomena(self):
 
         """
         Construct list of Phenomena based on variables in NetCDF file.
         :returns : List of metadata.product.Parameter objects.
         """
-
         try:
             with netCDF4.Dataset(self.file_path) as netcdf_object:
                 phens = []
@@ -39,6 +125,11 @@ class   NetCdfFile(GenericFile):
         Wrapper for method phenomena().
         :returns:  A dict containing information compatible with current es index level 2.
         """
+
+        #ok, lets try something new.
+        some_objects = self.get_geospatial()
+
+        some_other_ob = self.get_temporal()
 
         #Get basic file info.
         file_info = self.get_properties_generic_level1()
