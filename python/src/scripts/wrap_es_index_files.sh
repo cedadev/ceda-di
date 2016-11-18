25eb0cf97be76af0a1bd70cf402b3e5c206d611e
CEDA <CEDA@test.com>
2016-11-18 11:01:31 +0000

    Involved use of environment variable to define project.

--- python/src/scripts/wrap_es_index_files.sh
+++ python/src/scripts/wrap_es_index_files.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-cd /home/badc/software/datasets/sentinel/ceda-di-sentinel
+cd /home/badc/software/datasets/faam/elasticsearch
 . setup_env.sh
 
 DIRS=$@
 
+# Get project name from CEDA_DI_PROJECT environment variable
+if [ ! $CEDA_DI_PROJECT ]; then
+    echo "Please set CEDA_DI_PROJECT environment variable to use this script."
+    echo "E.g.: $ export CEDA_DI_PROJECT=faam"
+    exit
+fi
+
+proj=$CEDA_DI_PROJECT
+echo "Working on project: $proj"
+
 for dr in $DIRS; do
-    python scripts/es_index_files.py -i sentinel -d $dr
+    python scripts/es_index_files.py -i $proj -d $dr
 done
