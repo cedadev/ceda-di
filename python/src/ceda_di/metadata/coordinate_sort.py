3bf5dc5b86acf7f3aca005eabd5e7627cbbda671
rsmith013 <richard.d.smith@stfc.ac.uk>
2017-06-30 17:19:34 +0100

    Added two functions to handle polygons on the date line. If on the date line, script transports the polygon to the meridian for processing then translates it back. Makes sure that polygon is defined counter clockwise for ES. #33

--- python/src/ceda_di/metadata/coordinate_sort.py
+++ python/src/ceda_di/metadata/coordinate_sort.py
@@ -2,37 +2,42 @@
 
 '''                         
     Sort coordinates to counterclockwise order, and make sure the polygon is closed
-    Assumes coordinates are supplied as a list of  lat, lon (y,x) pairs.
+    
+    Assumptions:
+     - Coordinates are supplied as a list of  lat, lon (y,x) pairs.
+     - There is not a large polygon which spreads west -> east -175 lon -> 175 lon. All polygons are localised.
+
 '''
 
 from math import pi, atan2, sqrt
 
+
 def find_midpoint(coordinates):
     '''Remove duplicates and calculate average lon,lat'''
 
-    x = set([float(x[0]) for x in coordinates]) #lon
-    y = set([float(y[1]) for y in coordinates]) #lat
+    x = set([float(x[0]) for x in coordinates])  # lon
+    y = set([float(y[1]) for y in coordinates])  # lat
 
-    midx = sum(x)/len(x)
-    midy = sum(y)/len(y)
+    midx = sum(x) / len(x)
+    midy = sum(y) / len(y)
 
-    return [midx,midy]
+    return [midx, midy]
 
 
 def reference_vectors(coordinates, midpoint):
     '''Get the atan2 result for the reference vector. This is the first point from the coordinate list'''
 
     first_point = coordinates[0]
-    u = first_point[0] - midpoint[0] #lon
-    v = first_point[1] - midpoint[1] #lat
+    u = first_point[0] - midpoint[0]  # lon
+    v = first_point[1] - midpoint[1]  # lat
 
-    reference = atan2(v,u) #lat, lon
+    reference = atan2(v, u)  # lat, lon
     return reference
 
 
-def magnitude(u,v):
+def magnitude(u, v):
     '''Returns the vector magnitude'''
-    return sqrt(u**2 + v**2)
+    return sqrt(u ** 2 + v ** 2)
 
 
 def winding_angle(reference_vector, lon, lat):
@@ -60,20 +65,20 @@ def calculate_sorting_index(coordinates):
     reference = reference_vectors(coordinates, midpoint)
 
     index = []
-    for i,point in enumerate(coordinates):
+    for i, point in enumerate(coordinates):
 
-        u = point[0] - midpoint[0] #lon
-        v = point[1] - midpoint[1] #lat
+        u = point[0] - midpoint[0]  # lon
+        v = point[1] - midpoint[1]  # lat
 
-        angle = winding_angle(reference,u,v)
+        angle = winding_angle(reference, u, v)
 
         # constrain to be counter-clockwise
         if angle > 0:
             angle -= 2 * pi
 
-        distance = magnitude(u,v)
+        distance = magnitude(u, v)
 
-        index.append({'angle': angle,'distance':distance,'coordindex':i})
+        index.append({'angle': angle, 'distance': distance, 'coordindex': i})
 
     # Sort on ascending angle and decending distance
     index = sorted(index, key=lambda x: (-x['angle'], -x['distance']))
@@ -95,7 +100,7 @@ def calculate_sorting_index(coordinates):
 def sort_coords(coordinates):
     '''
     Takes the sorting index calculated by calculate_sorting_index() and returns the coordinates sorted as a closed polygon.
-    
+
     :param coordinates: list of [lon, lat] coordinate pairs.
     :return: a sorted list of [lon, lat] coordinate pairs
     '''
@@ -117,28 +122,28 @@ def ccw(coordinates):
 
     signedArea = 0
     end = len(coordinates)
-    for i,point in enumerate(coordinates):
+    for i, point in enumerate(coordinates):
         x1 = point[0]
         y1 = point[1]
-        if i == end-1:
+        if i == end - 1:
             x2 = coordinates[0][0]
             y2 = coordinates[0][1]
         else:
-            x2 = coordinates[i+1][0]
-            y2 = coordinates[i+1][1]
+            x2 = coordinates[i + 1][0]
+            y2 = coordinates[i + 1][1]
 
         signedArea += (x1 * y2 - x2 * y1)
 
     if signedArea > 0:
-        return True # counter-clockwise
+        return True  # counter-clockwise
     else:
-        return False # clockwise
+        return False  # clockwise
 
 
 def close_polygon(coordinates):
     '''
     Closes the polygon by duplicating the first coordinate pair and adding to the end.
-    
+
     :param coordinates: 
     :return: A closed polygon 
     '''
@@ -163,6 +168,52 @@ def filterDupes(coordinates):
     return coordinates
 
 
+def onDateLine(coordinates):
+    '''
+    Defines whether the polygon fed in, is on the international date line
+    :param coordinates: list of [lon, lat] coordinate pairs
+    :return: True  - The polygon crosses the date line
+             False - The polygon does not cross the dateline
+    '''
+    lon_filter = [pair[0] for pair in coordinates if pair[0] >= 175 or pair[0] <= -175]
+
+    # lon_filter returns empty list if the polygon is no where near the date line.
+    if not lon_filter:
+        return False
+
+    # lon_filter identified that the polygon is close to the date line.
+    # If there are positive and negative longitude values, the polygon crosses the dateline, else false.
+    east = 0
+    west = 0
+    for lon in lon_filter:
+        if lon > 0: east += 1
+        if lon < 0: west += 1
+        if east > 0 and west > 0:
+            return True
+
+    return False
+
+
+def translateCoordinates(coordinates):
+    '''
+    Translates the coordinates to and from the meridian for processing.
+    :param coordinates: list of [lon, lat] coordinate pairs
+    :return: coordinates list shifted to dateline or meridian.
+    '''
+    translated_coordinates = []
+
+    for pair in coordinates:
+        if pair[0] < 0:
+            pair[0] += 180
+
+        else:
+            pair[0] -= 180
+
+        translated_coordinates.append(pair)
+
+    return translated_coordinates
+
+
 def conditionPolygon(coordinates):
     '''
     Wrapper:
@@ -175,12 +226,26 @@ def conditionPolygon(coordinates):
     :return: sanitised coordinates, making sure it is a closed polygon and defined counter-clockwise
     '''
 
+    # check to see whether the polygon crosses the dateline
+    traverse_dateline = onDateLine(coordinates)
+
+    # If polygon crosses the dateline, translate the polygon to the meridian for processing.
+    if traverse_dateline:
+        coordinates = translateCoordinates(coordinates)
+
+    # Define counter-clockwise
     if not ccw(coordinates):
         coordinates = sort_coords(coordinates)
 
+    # Remove duplicate values
     coordinates = filterDupes(coordinates)
 
+    # Close polygon
     if not coordinates[0] == coordinates[-1]:
         coordinates = close_polygon(coordinates)
+
+    # If polygon crosses the dateline, translate the polygon back to the date line.
+    if traverse_dateline:
+        coordinates = translateCoordinates(coordinates)
 
     return coordinates
